c7e0e55ca0892d7a8644afd450b15127182e07be
Joshualeexy <oghenekevwejoshualee@gmail.com>
2025-04-14 10:30:58 +0100

    fix: remove redundant span and adjust userWalletDetails display in ModalHeader component

--- src/Components/ModalHeader.jsx
+++ src/Components/ModalHeader.jsx
@@ -3,6 +3,7 @@ import IconAngleLeft from './Icons/IconAngleLeft'
 import IconClose from './Icons/IconClose'
 
 const ModalHeader = ({ method1, method2, userWalletDetails, className }) => {
+    console.log("userWalletDetails", userWalletDetails)
     return (
         <div className={`flex justify-between px-4 mb-4 ${className}`}>
             <button onClick={method1} className="cursor-pointer">
@@ -12,9 +13,9 @@ const ModalHeader = ({ method1, method2, userWalletDetails, className }) => {
                 <img
                     src={userWalletDetails?.icon || '/Images/walletconnect.jpeg'}
                     className="w-8 h-8 rounded-full"
-                    alt={userWalletDetails?.name || 'wallet'}/>          
+                    alt={userWalletDetails?.name || 'wallet'}/>     {userWalletDetails?.name || 'Wallet Connect'}     
              </h4>
-             <span>{userWalletDetails?.name || 'Wallet Connect'}</span>
+             
 
             <button onClick={method2} className="cursor-pointer">
                 <IconClose />
